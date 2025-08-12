887cc7b425eef6325def466c43ca65daa69753de
FlorenceTo <Florenceto@live.co.uk>
2025-08-12 20:26:01 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const mapboxToken = "pk.eyJ1IjoidHlyeXgiLCJhIjoiY21lOHZ1eThvMGQ0MjJqcjBrbTR4eXd3NiJ9.xm7g9EZyYl9dnJWOSc7NlA";
+const mapboxgl.accessToken = "pk.eyJ1IjoidHlyeXgiLCJhIjoiY21lOHZ1eThvMGQ0MjJqcjBrbTR4eXd3NiJ9.xm7g9EZyYl9dnJWOSc7NlA";
 
 const map = L.map("map").setView([30, 0], 2);
 
